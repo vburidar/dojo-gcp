91752a6d219312980648c9b691c901412f1f1d42
Victor Buridard <victorburidard@gmail.com>
2021-12-15 12:00:15 +0100

    feat: add BDD connection to project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: ['dist/**/*.entity.js'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
