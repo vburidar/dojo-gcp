edccb33906b3469807c2ff43afacb59aecaca3e1
Victor Buridard <victorburidard@gmail.com>
2022-01-10 17:22:45 +0100

    feat: add routing + pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './Home'
+import About from './About'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-		<div>
-			Hello World
-		</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Routes>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
