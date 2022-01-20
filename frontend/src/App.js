915b6bae0cf095d632d0a60b3d591f52b294a869
Victor Lebrun <victor.lebrun011@gmail.com>
2022-01-20 18:33:04 +0100

    remove exact from route path matching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,15 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from './Home'
-import About from './About'
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Routes>
-          <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/" element={<Home/>}/>
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
