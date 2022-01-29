cb8abc55b989045e84f6beaffdf0d2e2a8253661
Victor Lebrun <victor.lebrun011@gmail.com>
2022-01-29 13:10:56 +0100

    add mapcrunch in home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,13 @@
 function Home() {
-    return (
-        <div>This is the Home Page</div>
-    )
+  return (
+    <iframe
+      id="inlineFrameExample"
+      title="Inline Frame Example"
+      width="100%"
+      height="100%"
+      src="https://www.mapcrunch.com/"
+    />
+  );
 }
 
 export default Home;
